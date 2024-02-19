1b1d011f7279081a89aa15ca5541f1b8304b59a5
Prince <prince.mishra@spit.ac.in>
2024-02-19 19:34:44 +0530

    count set bit

--- 231.cpp
+++ 231.cpp
@@ -4,14 +4,31 @@ using namespace std;
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
+        // method 1 -> divide by 2
+        // if(n<=0) return false;
 
-        while(n!=0){
-            if(n==1) return true;
-            if(n%2==0) n=n/2;
-            else return false;
-        }
-        return false;
+        // while(n!=0){
+        //     if(n==1) return true;
+        //     if(n%2==0) n=n/2;
+        //     else return false;
+        // }
+        // return false;
+
+        // divide by 2
+        // if(n<=0) return false;
+
+        // 16 -> 10000 
+        // count the set bit
+        // int x=1;
+        // int cnt=0;
+        // while(n){
+        //     if(n&x==1) cnt++;
+        //     n=n>>1; 
+        // }
+        // if(cnt==1) return true;
+        // return false;
+
+
     }
 };
 int main(){
