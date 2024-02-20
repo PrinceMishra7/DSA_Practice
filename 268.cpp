9065514ecaa70e1abc36e65df51e9b8c44404f9a
Prince <prince.mishra@spit.ac.in>
2024-02-20 19:59:27 +0530

    missing number solution

--- 268.cpp
+++ 268.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        long long int sum1=1LL*n*(n+1);
+        sum1=sum1/2;
+        long long int sum2=accumulate(nums.begin(),nums.end(),0);
+        return abs(sum1-sum2);
+    }
+};
 int main(){
     
     return 0;
