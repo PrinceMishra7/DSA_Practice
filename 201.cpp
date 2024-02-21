389aa56deb8c76063a82ec2c2c662c4ba69214c2
Prince <prince.mishra@spit.ac.in>
2024-02-21 21:48:53 +0530

    leetcode 201 solution bitwise and in range

--- 201.cpp
+++ 201.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+       
+       int cnt=0;
+       while(left!=right){
+           left=left>>1;
+           right=right>>1;
+           cnt++;
+       }
+       return left<<cnt;
 
+    }
+};
 int main(){
     
     return 0;
