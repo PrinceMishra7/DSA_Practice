be8237e0eb877d31888f81310e5d74012664769c
Prince <prince.mishra@spit.ac.in>
2024-02-25 21:47:49 +0530

    print prime factor of number

---
+++ printPrimeFactor.py
@@ -0,0 +1,19 @@
+n=int(input("Enter the number: "))
+
+
+# T.C. O(n)
+# for i in range(2,n+1):
+#     while n%i==0:
+#         print(i)
+#         n=n/i
+
+# if n>1 : print(n) // for test cases where n is prime number
+
+
+# T.C. O(sqrt(n))
+# for i in range(2,int(n**0.5)+1):
+#     while n%i==0:
+#         print(i)
+#         n=n/i
+
+# if n>1 : print(n) // for test cases where n is prime number or the left part is prime number(n=35)
