d5bcd97746f68a1ecead913f457985043930a808
Prince <prince.mishra@spit.ac.in>
2024-02-26 15:43:31 +0530

    leetcode 100 same tree

--- printPrimeFactor.py
+++ printPrimeFactor.py
@@ -1,6 +1,5 @@
 n=int(input("Enter the number: "))
 
-
 # T.C. O(n)
 # for i in range(2,n+1):
 #     while n%i==0:
@@ -17,3 +16,7 @@ n=int(input("Enter the number: "))
 #         n=n/i
 
 # if n>1 : print(n) // for test cases where n is prime number or the left part is prime number(n=35)
+
+# maximum number of prime factors that a number can have is log(n) to the base 2
+# assuming that each prime factor is 2 which is minimum prime number
+
