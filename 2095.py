34d310532689c9fbe5d042fd9898b8db82e5619b
Prince <prince.mishra@spit.ac.in>
2024-03-07 15:02:39 +0530

    delete middle node  of linkedlist

---
+++ 2095.py
@@ -0,0 +1,31 @@
+class Solution:
+    def middleNode(self,head:Optional[ListNode]) -> Optional[ListNode]:
+
+        if head==None:
+            return head
+        slow=head
+        fast=head
+
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+
+        return slow
+
+
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if head==None:
+            return head
+        
+        targetNode=self.middleNode(head)
+        if targetNode.next==None:
+            if targetNode==head:
+                return None
+            else:
+                head.next=None
+                return head
+        targetNode.val=targetNode.next.val
+        targetNode.next=targetNode.next.next
+
+        return head
