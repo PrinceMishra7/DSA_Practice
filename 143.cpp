afa6a81399fac87590b30d4c1d021f0a5b534620
Prince <prince.mishra@spit.ac.in>
2024-03-23 23:23:54 +0530

    reorder list solution

--- 143.cpp
+++ 143.cpp
@@ -1,7 +1,62 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+class Solution {
+public:
+    ListNode* findMiddle(ListNode* head){
 
+        if(head->next==NULL) return head;
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        /* fast!=NULL take cares of even length LL and fast->next!=NULL take cares of odd length LL*/
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL ||  head->next==NULL) return head;
+        ListNode* prev=NULL;
+
+        while(head!=NULL){
+            ListNode* ptr=head->next;
+            head->next=prev;
+            prev=head;
+            head=ptr;
+        }
+        return prev;
+    }
+    ListNode* mergeLL(ListNode* ll1,ListNode* ll2){
+        ListNode* first=ll1;
+        ListNode* second=ll2;
+
+        while(first!=NULL && second!=NULL){
+            ListNode* temp1=first->next;
+            first->next=second;
+            ListNode* temp2=second->next;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+        
+        return ll1;
+    }
+    void reorderList(ListNode* head) {
+
+        // find middle
+        ListNode* middle=findMiddle(head);
+        // reverse the right side of middle node
+        middle->next=reverseLL(middle->next);
+        // divide the linkedlist across middle
+        ListNode* ll2=middle->next;
+        middle->next=NULL;
+        ListNode* ll1=head;
+        // merge the left and right part
+        mergeLL(ll1,ll2);
+    }
+};
 int main(){
     
     return 0;
