001273776a5dba54e005b38c951594c70d9b85bd
Prince <prince.mishra@spit.ac.in>
2024-03-25 17:00:20 +0530

    find all duplicates in array

--- 442.cpp
+++ 442.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> freqArr(n+1,0);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(freqArr[nums[i]]==1){
+                freqArr[nums[i]]++;
+                ans.push_back(nums[i]);
+            }
+            else{
+                freqArr[nums[i]]++;
+            }
+        }
+        return ans;
+    }
+};
 int main(){
     
     return 0;
