5a6111fdbf9f316e0b285605c874aea8ef84fbd8
Prince <prince.mishra@spit.ac.in>
2024-03-27 23:31:07 +0530

    subarray product less than k

--- 713.cpp
+++ 713.cpp
@@ -1,7 +1,34 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int prod=1;
 
+        int ans=0;
+
+        while(j<n){
+            prod*=nums[j];
+
+            while(prod>=k && i<n && i<=j){
+                prod=prod/nums[i];
+                i++;
+            }
+
+            ans+=(j-i+1);
+            j++;
+
+        }
+
+        return ans;
+
+    }
+};
 int main(){
     
     return 0;
